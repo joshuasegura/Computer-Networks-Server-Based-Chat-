1600cc24be9a307a9c204053597a81a42bb984df
joshuasegura <47707284+joshuasegura@users.noreply.github.com>
2020-03-30 19:01:20 -0500

    Add files via upload
    Updated UDPClient and UDPServer files. The UDP authentication portion is basically done just need to polish a few things up. Just like the other files to compile javac UDPClient.java and javac UDPServer.java and then java UDPServer java UDPClient to run. Or run the shell script that I put in the command line(make sure all files in same directory).

--- UDPClient.java
+++ UDPClient.java
@@ -1,8 +1,17 @@
+import javax.xml.crypto.Data;
+import java.math.BigInteger;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 import java.io.IOException;
 
 public class UDPClient {
+
+    private static DatagramSocket d;
+    private static DatagramPacket DPreceived, DPsending;
+    private static byte[] received;
 
     public static void main(String[] args) throws Exception{
 	/**** This is the UDP client code ****/
@@ -12,23 +21,73 @@ public class UDPClient {
 	    // create the datagram socket
         DatagramSocket d = new DatagramSocket();
         InetAddress ip = InetAddress.getLocalHost();
-        byte buff[] = null;
+        String in, out;
+
+        System.out.println("This is just to get the UDP authentication working just type 'Log on' \n\tand" +
+                " follow the steps from there, the password for now is just password" );
+        out = input.nextLine();
 
         while(true){
-            String in = input.nextLine();
+            if(!out.equals("bye")){
+                if (out.equals("Log on")) {
+                    out = "HELLO(jas151530)";
+                }
 
-            // convert the string into a byte array so that it can be sent
-            buff = in.getBytes();
+                // sends the packet to the server
+                DPsending = new DatagramPacket(out.getBytes(),out.length(),ip,12000);
+                d.send(DPsending);
 
-            // creates the datagram packet that will be sent to the server
-            DatagramPacket DPSend = new DatagramPacket(buff, buff.length, ip, 12000);
-
-            // sends the actual datagram
-            d.send(DPSend);
-
-            if(in.trim().equals("bye"))
+                // buffer for data sent by the server
+                received = new byte[65535];
+                DPreceived = new DatagramPacket(received,received.length);
+                d.receive(DPreceived);
+                in = new String(DPreceived.getData()).trim();
+                if(in.contains("CHALLENGE(")){
+                    int end = in.length();
+                    System.out.println("Enter Password: ");
+                    String password = input.nextLine();
+                    String rand = in.substring(10,end-1);
+                    String res = A3(rand, password);
+                    out = "RESPONSE(" + res + ")";
+                    DPsending = new DatagramPacket(out.getBytes(),out.length(),ip,12000);
+                    d.send(DPsending);
+                }
+                if(in.contains("AUTH_FAIL")){
+                    System.out.println("Incorrect password .... exiting");
+                    break;
+                }
+                if(in.contains("AUTH_SUCCESS(")){
+                    System.out.println("Password correct");
+                    System.out.println("\tStill need to create the cookie and port number part here\n" +
+                            "\tas well as create the encrypted channel for communication -JS");
+                    break;
+                }
+                System.out.println("Server: " + in);
+            }
+            else
                 break;
         }
+        d.close();
         System.out.println("out of client loop");
+    }
+
+    private static String A3(String rand, String password) throws NoSuchAlgorithmException {
+        String hash = rand + password;
+        System.out.println("Hashing: " + hash);
+        // using SHA-256 for this hash
+        MessageDigest m = MessageDigest.getInstance("SHA-256");
+        m.update(hash.getBytes(StandardCharsets.UTF_8));
+        byte[] hashed = m.digest();
+
+        // convert the hashed result back into a String
+        BigInteger num = new BigInteger(1,hashed);
+        StringBuilder hex = new StringBuilder(num.toString(16));
+        while(hex.length() < 32){
+            hex.insert(0,'0');
+        }
+
+        String done = hex.toString();
+
+        return done;
     }
 }
--- UDPServer.java
+++ UDPServer.java
@@ -1,33 +1,84 @@
+import java.math.BigInteger;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 
 public class UDPServer {
+
+    private static DatagramSocket d;
+    private static DatagramPacket DPreceived,DPsending;
+    private static byte[] received;
+    private static String real_user = "jas151530";
+    private static String password = "password";
+    private static String Xres;
 
     public static void main(String[] args) throws Exception{
         /**** This is the server for the chat system  ****/
 
         // new datagram socket at port 12000
-        DatagramSocket d = new DatagramSocket(12000);
-        byte[] receive = new byte[65535]; // buffer to hold the incoming message
-
-        DatagramPacket DpReceived = null;
+        d = new DatagramSocket(12000);
+        String in,out = null;
+        InetAddress client_Address = null;
+        int client_port;
 
         while(true){
-            // creates a datagram packet that receives data
-            DpReceived = new DatagramPacket(receive, receive.length);
-            d.receive(DpReceived);
+            received = new byte[65535];
+            DPreceived = new DatagramPacket(received, received.length);
+            d.receive(DPreceived);
+            client_Address = DPreceived.getAddress();
+            client_port = DPreceived.getPort();
+            in = new String(DPreceived.getData()).trim();
+            if(in.contains("HELLO(")){
+                int end = in.length();
+                String validate_user = in.substring(6,end-1);
+                if(validate_user.equals(real_user)){
+                    Random random = new Random();
+                    int rand = random.nextInt(); // generates the random number
+                    Xres = A3(rand, password);
+                    out = "CHALLENGE(" + rand + ")";
+                }
+                else
+                    out = validate_user + " isn't a valid user";
+            }
+            else if(in.contains("RESPONSE(")){
+                String res = in.substring(9,in.length()-1);
+                if(res.equals(Xres) ){
+                    out = "AUTH_SUCCESS(rand_cookie, port_number)";
+                }
+                else
+                    out = "AUTH_FAIL";
+            }
 
-            String str = new String(receive);
-            if(str.trim().equals("bye")){
-                System.out.println("Client sent bye closing the connection");
+            if(in.contains("bye"))
                 break;
-            }
-            else{
-                System.out.println(str);
-            }
 
-            receive = new byte[65535];
-
+            System.out.println(out);
+            DPsending = new DatagramPacket(out.getBytes(),out.length(),client_Address,client_port);
+            d.send(DPsending);
         }
         System.out.println("out of server loop");
+    }
+
+    // This is the authentication algorithm for the UDP user verification
+    private static String A3(int rand, String password) throws NoSuchAlgorithmException {
+        String hash = Integer.toString(rand) + password;
+        System.out.println("Hashing: " + hash);
+        // using SHA-256 for this hash
+        MessageDigest m = MessageDigest.getInstance("SHA-256");
+        m.update(hash.getBytes(StandardCharsets.UTF_8));
+        byte[] hashed = m.digest();
+
+        // convert the hashed result back into a String
+        BigInteger num = new BigInteger(1,hashed);
+        StringBuilder hex = new StringBuilder(num.toString(16));
+        while(hex.length() < 32){
+            hex.insert(0,'0');
+        }
+
+        String done = hex.toString();
+
+        return done;
     }
 }
